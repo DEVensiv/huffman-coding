a5c87d924563294e43fdb882ae2003af1da227b0
devensiv <devensiv@devensiv.dev>
2023-12-24 14:28:47 +0100

    fix: more pessimistic benchmarking

--- benches/decoding.rs
+++ benches/decoding.rs
@@ -1,18 +1,18 @@
 use std::fs::OpenOptions;
 
 use criterion::{criterion_group, criterion_main, Criterion};
-use huffman::hencode;
+use huffman::hdecode;
 use tempfile::tempfile;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("encode", |bencher| {
+    c.bench_function("decode", |bencher| {
         bencher.iter(|| {
             let mut out = tempfile().expect("temfile err");
             let mut raw = OpenOptions::new()
                 .read(true)
-                .open("flake.lock")
+                .open("flake.lock.rxc")
                 .expect("file err");
-            hencode(&mut raw, &mut out).expect("io err");
+            hdecode(&mut raw, &mut out).expect("io err");
         })
     });
 }
