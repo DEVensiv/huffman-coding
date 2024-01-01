8f4abbd4289151019fae617c3fe2a87a0226a5cd
devensiv <devensiv@devensiv.dev>
2024-01-01 22:21:06 +0100

    bench: improve window_primitives bench

--- benches/window_primitives.rs
+++ benches/window_primitives.rs
@@ -3,10 +3,12 @@ use std::time::Duration;
 use criterion::{criterion_main, BenchmarkId, Criterion};
 use huffman::window::BitWindow;
 
-const SOURCE_BYTES: usize = 1000 * 3;
+const SOURCE_BYTES: usize = 40;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("show_u8", |bencher| {
+
+    let mut show = c.benchmark_group("show");
+    show.bench_function("show_u8", |bencher| {
         bencher.iter(|| {
             let slice_of_u8: &[u8] = &[0b1000_1111; SOURCE_BYTES];
             let reader: BitWindow<_> = slice_of_u8.into();
@@ -14,52 +16,64 @@ fn criterion_benchmark(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("show_exact(8)", |bencher| {
+    show.bench_function("show_exact(8)", |bencher| {
         bencher.iter(|| {
             let slice_of_u8: &[u8] = &[0b1000_1111; SOURCE_BYTES];
             let reader: BitWindow<_> = slice_of_u8.into();
             reader.show_exact(8)
         })
     });
+    show.finish();
 
-    let mut show_diff = c.benchmark_group("show_exact(diff)");
+    let mut show_exact = c.benchmark_group("show_exact(diff)");
     for input in 1..8 {
-        show_diff.bench_with_input(
-            BenchmarkId::new("show_exact(diff)", input),
-            &input,
+        show_exact.bench_with_input(BenchmarkId::new("", input), &input, |bencher, input| {
+            bencher.iter(|| {
+                let slice_of_u8: &[u8] = &[0b1000_1111; SOURCE_BYTES];
+                let reader: BitWindow<_> = slice_of_u8.into();
+                reader.show_exact(*input)
+            })
+        });
+    }
+    show_exact.finish();
+
+    let mut consume = c.benchmark_group("consume_small");
+    for consumes in 0..5 {
+        consume.bench_with_input(
+            BenchmarkId::new("", consumes),
+            &consumes,
             |bencher, input| {
                 bencher.iter(|| {
                     let slice_of_u8: &[u8] = &[0b1000_1111; SOURCE_BYTES];
-                    let reader: BitWindow<_> = slice_of_u8.into();
-                    reader.show_exact(*input)
+                    let mut reader: BitWindow<_> = slice_of_u8.into();
+                    for _ in 0..*input {
+                        reader.consume(8).unwrap();
+                    }
+                    reader
                 })
             },
         );
     }
-    show_diff.finish();
-
-    c.bench_function("consume", |bencher| {
-        bencher.iter(|| {
-            let slice_of_u8: &[u8] = &[0b1000_1111; SOURCE_BYTES];
-            let reader: BitWindow<_> = slice_of_u8.into();
-            consume(reader, slice_of_u8.len())
-        })
-    });
-}
-
-fn consume(mut reader: BitWindow<&[u8]>, bytes: usize) {
-    for _ in 0..(bytes / 3) {
-        // You obviously should use try! or some other error handling mechanism here
-        reader.consume(1).unwrap();
-        reader.consume(2).unwrap();
-        reader.consume(3).unwrap();
-        reader.consume(6).unwrap();
-
-        reader.consume(1).unwrap();
-        reader.consume(2).unwrap();
-        reader.consume(3).unwrap();
-        reader.consume(6).unwrap();
+    consume.finish();
+    let mut consume = c.benchmark_group("consume_big");
+    for consumes in 1..5 {
+        let consumes = consumes * 10;
+        consume.bench_with_input(
+            BenchmarkId::new("", consumes),
+            &consumes,
+            |bencher, input| {
+                bencher.iter(|| {
+                    let slice_of_u8: &[u8] = &[0b1000_1111; SOURCE_BYTES];
+                    let mut reader: BitWindow<_> = slice_of_u8.into();
+                    for _ in 0..*input {
+                        reader.consume(8).unwrap();
+                    }
+                    reader
+                })
+            },
+        );
     }
+    consume.finish();
 }
 
 pub fn benches() {
